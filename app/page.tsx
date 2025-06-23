48e5cac34051e70dea53c396bf912b179bff62ed
LAPTOP-NS71Q3TJ\epske <etienne.skein@thedatacompany.com>
2025-06-24 00:28:51 +0200

    Styling with background pattern added

--- app/page.tsx
+++ app/page.tsx
@@ -24,11 +24,29 @@ export default function Home() {
     <div className="min-h-screen bg-gray-50 relative overflow-hidden">
       <NavBar />
       <HeroSection />
-      <div className="relative z-10 space-y-32 pb-20">
-        <UnifiedBenefitsFeaturesSection />
-        <IPhoneShowcaseSection />
-        <FaqSection />
-        <CtaSection />
+      <div className="relative z-10 pb-20">
+        <div className="relative">
+          <div
+            className="absolute top-[-8rem] bottom-[-8rem] left-0 right-0 z-[-1]"
+            style={{
+              maskImage: 'linear-gradient(to bottom, transparent, black 8rem, black calc(100% - 8rem), transparent)',
+            }}
+          >
+            <div
+              className="absolute inset-0 opacity-[0.03]"
+              style={{
+                backgroundImage: "url(\"data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23000' fill-opacity='1'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E\")",
+              }}
+            ></div>
+          </div>
+          
+          <div className="space-y-32">
+             <UnifiedBenefitsFeaturesSection />
+             <IPhoneShowcaseSection />
+             <FaqSection />
+             <CtaSection />
+          </div>
+        </div>
       </div>
       <Footer />
       <FloatingElements />
